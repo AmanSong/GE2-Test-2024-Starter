1685095f1a622ce13fd76a07115a43a7991f1a34
AmanSong <amansong70@gmail.com>
2024-04-23 10:29:29 +0100

    draw some cubes

--- GE2-Test-Godot/behaviors/creature_generator.gd
+++ GE2-Test-Godot/behaviors/creature_generator.gd
@@ -3,18 +3,18 @@ extends Node3D
 
 # Adding fields length, frequency, start_angle, base_size, multiplier
 @export var length  = 1
-@export var frequency  = 0
-@export var start_angle  = 0
-@export var base_size = 0
-@export var multiplier = 0
+@export var frequency  = 1
+@export var start_angle  = 1
+@export var base_size = 3
+@export var multiplier = 1
 
 var force = Vector3.ZERO
 
 func _process(delta):
 	# draw gizmo segments
+	var pos = transform.origin + Vector3(0, 0, base_size)
 	for i in length:
-		DebugDraw3D.draw_box(global_transform.origin, Quaternion.IDENTITY,  Vector3(3, 3, 3), Color.DARK_GOLDENROD, true)
-		DebugDraw3D.draw_box(global_transform.origin / 3, Quaternion.IDENTITY,  Vector3(30, 3, 3), Color.DARK_GOLDENROD, true)
+		DebugDraw3D.draw_box(pos * i, Quaternion.IDENTITY,  Vector3(base_size, base_size, base_size), Color.DARK_GOLDENROD, true)
 
 func _ready():
 	if not Engine.is_editor_hint():		
